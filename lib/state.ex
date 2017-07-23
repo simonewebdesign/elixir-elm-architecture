6eba422253e6ea004f7770b7219563a70082ba26
Simone Vittori <hello@simonewebdesign.it>
2017-07-23 16:15:35 +0100

    Implementation of Server

--- lib/state.ex
+++ lib/state.ex
@@ -2,23 +2,43 @@ defmodule State do
   @moduledoc """
   Collects all the triggered messages and ensures they are executed
   in the correct order.
+
+  It also keeps track of the current application state and all the previous ones.
+
+  For example this is how the GenServer state will look like:
+
+      %{module: module,      # The application module name, e.g. Counter
+        frames: [
+          %{message: _,      # Each "frame" is a map containing the
+            model: _},       # application state and the message that
+          %{message: _,      # produced that state.
+            model: _},
+          ...
+        ],
+        initial_model: _     # The application's initial model
+      }
   """
   use GenServer
+
 
   # Client API
 
   # def start_link(%{model: model, view: _v, update: _u} = config) do
-    def start_link(module) do
-    GenServer.start_link(__MODULE__, module.model)
+  def start_link(module) do
+    GenServer.start_link(__MODULE__, %{
+      module: module,
+      frames: [],
+      initial_model: module.model()
+    })
   end
 
   def step(pid, msg) do
     GenServer.call(pid, {:step, msg})
   end
 
-  # def undo(pid, msg) do
-
-  # end
+  def undo(pid) do
+    GenServer.call(pid, :undo)
+  end
 
   # def push(pid, evt) do
     # GenServer.call(pid, {:push, evt})
@@ -31,19 +51,37 @@ defmodule State do
   # Server (callbacks)
 
   def handle_call({:step, msg}, _from, state) when msg in [:Increment, :Decrement] do
-    # new_state = %{state | model: state.update(msg, state.model)}
-    new_state = apply(Counter, :update, [msg, state])
+    new_model = update_model(msg, state)
+    new_frame = %{message: msg, model: new_model}
 
-    {:reply, {:ok, new_state}, new_state}
+    {:reply, {:ok, new_model}, %{state | frames: [new_frame|state.frames]}}
   end
 
   def handle_call({:step, _msg}, _from, state) do
     {:reply, {:error, :invalid_message}, state}
   end
 
+
+  def handle_call(:undo, _from, state) do
+    {previous_model, frames} = undo_model(state)
+    {:reply, {:ok, previous_model}, %{state | frames: frames}}
+  end
+
   def handle_call(:pop, _from, [h | t]) do
     {:reply, h, t}
   end
+
+  # Private
+
+  defp update_model(msg, %{module: mod, frames: [], initial_model:  model}) do
+    apply mod, :update, [msg, model]
+  end
+  defp update_model(msg, %{module: mod, frames: [head|_], initial_model: _}) do
+    apply mod, :update, [msg, head.model]
+  end
+
+  defp undo_model(%{frames: [ _ | [head|tail] ]}), do: {head.model, tail}
+  defp undo_model(%{frames: [], initial_model: model}), do: {model, []}
 
   # def handle_call(request, from, state) do
   #   # Call the default implementation from GenServer
--- test/state_test.exs
+++ test/state_test.exs
@@ -3,23 +3,61 @@ defmodule StateTest do
 
   setup do
     {:ok, pid} = State.start_link(Counter)
-    {:ok, state: pid}
+    {:ok, server: pid}
   end
 
-  test "Initial state", %{state: pid} do
-    assert :sys.get_state(pid) == 0
+  test "Initial state", %{server: pid} do
+    assert :sys.get_state(pid) ==
+      %{
+        module: Counter,
+        frames: [],
+        initial_model: 0
+      }
   end
 
-  test "Update the state", %{state: pid} do
-    {:ok, new_state} = State.step(pid, :Increment)
+  test "Update the model", %{server: pid} do
+    {:ok, new_model} = State.step(pid, :Increment)
 
-    assert new_state == 1
+    assert new_model == 1
   end
 
-  test "An invalid message should produce an error and state should be unchanged", %{state: pid} do
+  test "An invalid message should produce an error and state should be unchanged", %{server: pid} do
     {:error, error_message} = State.step(pid, :DefinitelyNotAValidMessage)
 
     assert error_message == :invalid_message
-    assert :sys.get_state(pid) == 0
+    assert :sys.get_state(pid) ==
+      %{
+        module: Counter,
+        frames: [],
+        initial_model: 0
+      }
+  end
+
+  test "Undoing the state preserves the order", %{server: pid} do
+    {:ok, _new_model} = State.step(pid, :Increment) # 1
+    {:ok, _new_model} = State.step(pid, :Decrement) # 0
+    {:ok, new_model} = State.step(pid, :Increment)  # 1
+
+    assert new_model == 1
+
+    {:ok, old_model} = State.undo(pid)
+
+    assert old_model == 0
+  end
+
+  test "Undoing the initial state is a no-op", %{server: pid} do
+    {:ok, _new_model} = State.step(pid, :Increment)
+    {:ok, new_model} = State.step(pid, :Decrement)
+
+    assert new_model == 0
+
+    {:ok, old_model} = State.undo(pid)
+
+    assert old_model == 1
+
+    {:ok, old_model}   = State.undo(pid)
+    {:ok, older_model} = State.undo(pid)
+
+    assert old_model == older_model
   end
 end
